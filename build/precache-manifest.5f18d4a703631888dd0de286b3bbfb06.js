e99b2c31276a55276ebcc07baef1c009e8d2f59c
harshmarwah007 <40794170+harshmarwah007@users.noreply.github.com>
2021-07-06 17:21:44 +0530

    first commit

---
+++ build/precache-manifest.5f18d4a703631888dd0de286b3bbfb06.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33e8245bee6f3c840eade681e3b74c68",
+    "url": "/Todo-List-ReactJS/index.html"
+  },
+  {
+    "revision": "9302e28affcbd310209f",
+    "url": "/Todo-List-ReactJS/static/js/2.17e1ab02.chunk.js"
+  },
+  {
+    "revision": "8a32d68dcb8f0fb2683b",
+    "url": "/Todo-List-ReactJS/static/js/main.85847575.chunk.js"
+  },
+  {
+    "revision": "db6565fd58a0807ce78d",
+    "url": "/Todo-List-ReactJS/static/js/runtime-main.30a4f002.js"
+  }
+]);
